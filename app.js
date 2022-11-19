a9918ccc7afee34b868521f0b60c43f641e5502e
Shangsit <shangsitnath01@gmail.com>
2022-11-19 23:14:32 +0530

    Revert "Revert "basic error handling done""
    This reverts commit 8ed2dedffe969c40b9319d3b31b35f9f0e500f5d.

---
+++ app.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const { findByIdAndDelete } = require('./models/campground');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const {campgroundSchema} = require('./schemas');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+
+    // I do not know why the following lines are giving error??
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express()
+
+app.engine('ejs', ejsMate);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+    // Here Joi is a npm package which is used for server side validation. The below schema is moved to a 'schemas' file.
+    // const campgroundSchema = Joi.object({
+    //     campground: Joi.object({
+    //         title: Joi.string().required(),
+    //         price: Joi.number().required().min(0),
+    //         image: Joi.string().required(),
+    //         location: Joi.string().required(),
+    //         description: Joi.string().required()
+    //     }).required()
+    // })
+    const result = campgroundSchema.validate(req.body);
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        console.log(result);
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/campgrounds', catchAsync(async (req, res) => {
+    const camp = await Campground.find({});
+    res.render('campgrounds/campground', { camp });
+}))
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // here we have added server side validation as a middleware named 'validateCampground'
+    const newcampground = new Campground(req.body.campground);
+    await newcampground.save();
+    res.redirect(`/campgrounds/${newcampground._id}`); //why address is starting with '/' ????
+    console.log("successfully posted!!");
+}))
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render('campgrounds/show', { camp });
+}))
+
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render('campgrounds/edit', { camp });
+}))
+
+app.get('/makecampground', catchAsync(async (req, res) => {
+    const camp = new Campground({ title: 'My Yard', description: 'Cheap camping!' });
+    await camp.save();
+    res.send(camp);
+}))
+
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
+    const camp = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${camp._id}`); //why address is starting with '/' ????
+}))
+
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`);
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) {
+        err.message = "Something went wrong!";
+    }
+    res.status(statusCode).render('error', { err });
+    console.log("something went wrong!!")
+})
+
+app.listen('3000', () => {
+    console.log('Server running at port 3000')
+})
